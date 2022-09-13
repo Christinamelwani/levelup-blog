c33faf44f8bc0e54276ce0a5139cd4f6386cd8ef
Domagoj Franc <domagoj.franc@gmail.com>
2022-09-13 12:41:51 +0200

    Main article version one

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,20 @@
 
 <template>
   <main>
-    <h1>Home</h1>
+    <article class="mainArticle">
+      <div class="mainArticle__inner">
+        <ul class="mainArticle__categories">
+          <li class="mainArticle__category">
+            <RouterLink to="/" class="mainArticle__category-link">Category</RouterLink>
+          </li>
+        </ul>
+        <h2 class="mainArticle__heading">Lorem ipsum dolor.</h2>
+        <p class="mainArticle__content">
+          <time class="mainArticle__content-time">10:00</time>
+          <span class="mainArticle__content-divider"></span>
+          <span class="mainArticle__content-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</span>
+        </p>
+      </div>
+    </article>
   </main>
 </template>
