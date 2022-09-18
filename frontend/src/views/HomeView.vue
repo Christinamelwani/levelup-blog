8bea096f0e9b20fb4fcf9440331eb60b3100af93
root <root@DESKTOP-U6UMG26.localdomain>
2022-09-18 17:29:08 +0700

    Blog Slider and header done

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,5 +2,45 @@
 
 <template>
   <main>
+    <section class="blogSlider">
+      <div class="blogSlider__inner">
+        <div class="blogSlider__category">
+          Adventure
+        </div>
+        <div class="blogSlider__header">
+          Richird Norton photorealistic rendering as real photos
+        </div>
+        <div class="blogSlider__text">
+          <div class="blogSlider__text-subtext">
+            08.08.2021
+          </div>
+          <svg class="blogSlider__text-line" width="60" height="1" viewBox="0 0 30 1" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <line x1="1.7418e-10" y1="0.5" x2="30" y2="0.5" stroke="#E5E5E5" />
+          </svg>
+          <div class="blogSlider__text-subtext">
+            Progressively incentivize cooperative systems through technically sound functionalities. The credibly
+            productivate seamless data.
+          </div>
+        </div>
+      </div>
+      <div class="blogSlider__pagination">
+        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="4" cy="4" r="4" fill="white" />
+        </svg>
+        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="4" cy="4" r="4" fill="white" fill-opacity="0.2"/>
+</svg>
+
+<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="4" cy="4" r="4" fill="white" fill-opacity="0.2"/>
+</svg>
+
+      </div>
+    </section>
   </main>
 </template>
+
+<style lang="scss">
+  @import '../assets/scss/main';
+</style>
