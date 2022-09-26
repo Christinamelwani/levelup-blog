ed8f37d042f7bf230c9175b84694633ccfcd56a7
christina.melwani <christina.melwani@internetbrands.com>
2022-09-26 21:56:56 +0700

    Slider done at long last

--- frontend/src/components/Slider.vue
+++ frontend/src/components/Slider.vue
@@ -6,16 +6,16 @@ import CategoryCard from './CategoryCard.vue';
 <template>
     <section class="slider">
         <div class="slider__inner">
-            <CategoryCard class="categoryCard-slider" />
+            <CategoryCard :class="`categoryCard-${$route.name}`" />
             <div class="slider__header">
                 Richird Norton photorealistic rendering as real photos
             </div>
             <div class="slider__text">
-                <div class="slider__subtext">
+                <div v-if="$route.name === 'home'" class=" slider__subtext">
                     08.08.2021
                 </div>
-                <svg class="slider__line" width="30" height="1" viewBox="0 0 30 1" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                <svg v-if="$route.name === 'home'" class=" slider__line" width="30" height="1" viewBox="0 0 30 1"
+                    fill="none" xmlns="http://www.w3.org/2000/svg">
                     <line x1="1.7418e-10" y1="0.5" x2="30" y2="0.5" stroke="#E5E5E5" />
                 </svg>
                 <div class="slider__subtext">
@@ -24,7 +24,7 @@ import CategoryCard from './CategoryCard.vue';
                     productivate seamless data.
                 </div>
             </div>
-            <div class="slider__pagination">
+            <div v-if="$route.name === 'home'" class="slider__pagination">
                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="4" cy="4" r="4" fill="white" />
                 </svg>
@@ -35,9 +35,9 @@ import CategoryCard from './CategoryCard.vue';
                     <circle cx="4" cy="4" r="4" fill="white" fill-opacity="0.2" />
                 </svg>
             </div>
-            <!-- <div class="slider__byline">
+            <div v-if="$route.name === 'article'" class="slider__byline">
                 By Jennifer Lawrence
-            </div> -->
+            </div>
         </div>
     </section>
 </template>
--- frontend/src/views/ArticleView.vue
+++ frontend/src/views/ArticleView.vue
@@ -1,28 +1,28 @@
 <script setup>
 import Slider from "../components/Slider.vue";
 import HighlightedArticleCard from "../components/HighlightedArticleCard.vue";
-import Byline from "../components/Byline.vue";</script>
+import Byline from "../components/Byline.vue";
+</script>
 
 <template>
     <main>
-        <Slider />
-        <section class="articleBody">
-            <div class="articleBody__content">
-                <div class="articleBody__col1">
-                    <div class="articleBody__col1__wrapper">
-                        <date class="articleBody__col1-item">
-                            08.08.2021
-                        </date>
-                        <svg width="35" height="1" viewBox="0 0 35 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <line y1="0.5" x2="35" y2="0.5" stroke="#C4C4C4" />
-                        </svg>
-                        <div class="articleBody__col1-item">
-                            4 minutes
-                        </div>
+        <Slider class="slider-article" />
+        <section class="article">
+            <div class="article__body">
+                <div class="article__aside">
+                    <date class="article__date">
+                        08.08.2021
+                    </date>
+                    <svg class="article__line" width="35" height="1" viewBox="0 0 35 1" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <line y1="0.5" x2="35" y2="0.5" stroke="#C4C4C4" />
+                    </svg>
+                    <div class="article__date">
+                        4 minutes
                     </div>
                 </div>
-                <div class=" articleBody__col2">
-                    <p class="articleBody__col2__text">
+                <div class="article__wrapper">
+                    <p class="article__text">
                         Seamlessly syndicate cutting-edge architectures rather than collaborative collaboration and
                         idea-sharing. Proactively incubate visionary interfaces whereas premium benefits. Seamlessly
                         negotiate ubiquitous leadership skills rather than parallel ideas. Dramatically visualize
@@ -38,7 +38,7 @@ import Byline from "../components/Byline.vue";</script>
                         revolutionary schemas. Progressively network just in time customer service without real-time
                         scenarios.
                     </p>
-                    <p class="articleBody__col2__text">
+                    <p class="article__text">
                         Synergistically drive e-business leadership with unique synergy. Compellingly seize market
                         positioning ROI and bricks-and-clicks e-markets. Proactively myocardinate timely platforms
                         through
@@ -53,24 +53,24 @@ import Byline from "../components/Byline.vue";</script>
                         transition virtual outsourcing with focused e-tailers.
 
                     </p>
-                    <h2 class="articleBody__col2__quote">
+                    <h2 class="article__quote">
                         “ Monotonectally seize superior mindshare rather than efficient technology. ”
                     </h2>
-                    <p class="articleBody__col2__text">
+                    <p class="article__text">
                         Compellingly enhance seamless resources through competitive content. Continually actualize
                         24/365
                         alignments for resource-leveling platforms. Energistically enhance high standards in models and
                         professional expertise. Intrinsicly iterate extensible metrics for prospective opportunities.
                         Continually develop leading-edge experiences through quality e-services.
                     </p>
-                    <div class="articleBody__categories">
-                        <div class="articleBody__categories__card">
+                    <div class="article__categories">
+                        <div class="article__category">
                             Adventure
                         </div>
-                        <div class="articleBody__categories__card">
+                        <div class="article__category">
                             Photo
                         </div>
-                        <div class="articleBody__categories__card">
+                        <div class="article__category">
                             Design
                         </div>
                     </div>
@@ -86,9 +86,7 @@ import Byline from "../components/Byline.vue";</script>
                 Related Posts
             </h1>
             <div class="editorsPick__content">
-                <div class="editorsPick__content__card" v-for="index in 3">
-                    <HighlightedArticleCard />
-                </div>
+                <HighlightedArticleCard v-for="index in 3" />
             </div>
         </section>
     </main>
