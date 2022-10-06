590a532dc4f38e261f6db8434188792053963b22
christina.melwani <christina.melwani@internetbrands.com>
2022-10-06 12:09:48 +0700

    Created login modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import Modal from './components/Modal.vue';
+export default {
+  data() {
+    return {
+      collapseModal: false
+    }
+  },
+  methods: {
+    toggleCollapseModal() {
+      this.collapseModal = !this.collapseModal
+    }
+  },
+  components: { Header, Footer, Modal }
+}
+
 </script>
 
 <template>
-  <Header> </Header>
+  <Header @collapse-modal="toggleCollapseModal"> </Header>
+  <Modal @collapse-modal="toggleCollapseModal" v-if="collapseModal" />
   <RouterView />
   <Footer> </Footer>
 </template>
